27a531a8d78e047ec2aa8ed5b90c14bdd0b91c9e
John Bergandino <jbergandino@gmail.com>
2016-03-18 14:52:34 -0400

    iOS stopwatch application built with Swift 2.0 and Xcode

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Time Variables
+    var time = 0.00
+    
+    @IBOutlet weak var isEnabled: UIBarButtonItem!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = NSTimer()
+    
+    @IBAction func startButton(sender: AnyObject) {
+        
+        isEnabled.enabled = false
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        
+        isEnabled.enabled = true
+        timer.invalidate()
+    }
+    
+    @IBAction func resetButton(sender: AnyObject) {
+    
+        isEnabled.enabled = true
+        timer.invalidate()
+        time = 0.00
+        timerLabel.text = "0.00"
+    }
+    
+    func increaseTimer(){
+    
+        time++
+        timerLabel.text = "\(time/100)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
